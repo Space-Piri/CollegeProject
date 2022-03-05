1e590f3e9f6d8e5dc7c1048abd949474d8d353de
Space-Piri <65462639+Space-Piri@users.noreply.github.com>
2022-03-05 14:06:58 +0300

    add repositories

---
+++ CollegeServer/Repositories/TeachersRepository.cs
@@ -0,0 +1,25 @@
+﻿using CollegeServer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CollegeServer.Repositories
+{
+    public class TeachersRepository : Repository
+    {
+        public static Teachers GetTeachersById(int Id)
+        {
+            SqlConnection cn = new SqlConnection(ConnectionString);
+            cn.Open();
+            SqlCommand cmd = new SqlCommand("select * from Teachers where Id  = " + Id, cn);
+            var reader = cmd.ExecuteReader();
+            Teachers teacherS = null;
+            while (reader.Read())
+            {
+                teacherS = new
+            }
+        }
+    }
+}
---
+++ CollegeServer/Repositories/UserRepository.cs
@@ -0,0 +1,28 @@
+﻿using CollegeServer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CollegeServer.Repositories
+{
+    public class UserRepository : Repository
+    {
+        public static List<Users> GetUsers()
+        {
+            SqlConnection cn = new SqlConnection(ConnectionString);
+            cn.Open();
+            SqlCommand cmd = new SqlCommand("select * from Users", cn);
+            var reader = cmd.ExecuteReader();
+            List<Users> userS = null;
+            while (reader.Read())
+            {
+                userS.Add(new Users(Convert.ToInt32(reader["Id"]), Convert.ToString(reader["FIO"]), UserTypesRepository.GetUserTypesById(Convert.ToInt32(reader["Id_Type"])), Convert.ToString(reader["Login"]), Convert.ToString(reader["Password"])));
+            }
+
+            cn.Close();
+            return userS;
+        }
+    }
+}
